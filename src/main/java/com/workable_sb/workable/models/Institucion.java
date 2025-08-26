988d9f29d26f966c0b1fc7313a938d2ff2847c05
NicolasZeq2722 <nicomansolov89@gmail.com>
2025-08-26 16:07:50 -0500

    Nuevo

--- src/main/java/com/workable_sb/workable/models/Institucion.java
+++ src/main/java/com/workable_sb/workable/models/Institucion.java
@@ -1,5 +1,31 @@
 package com.workable_sb.workable.models;
 
-public class Institucion {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table (name = "institucion")
+
+public class Institucion {
+    @Id
+    @Column(name = "institu_id")
+    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    private Integer institu_id;
+    
+    @Column(nullable = false, length = 255)
+    private String nombre;
+
+    @Column(nullable = false, length = 255)
+    private String ciudad;
+    
+    @Column(nullable = false, length = 255)
+    private String departam;
+
+    @Column(nullable = false, length = 255)
+    private String pais;
 }
--- src/main/java/com/workable_sb/workable/models/NivelEducativo.java
+++ src/main/java/com/workable_sb/workable/models/NivelEducativo.java
@@ -1,5 +1,20 @@
 package com.workable_sb.workable.models;
 
-public class NivelEducativo {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "niveleducativo")
+public class NivelEducativo {
+    @Id
+    @Column(name = "niveleducativo")
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer n_edu_id;
+
+    @Column(nullable = false, length = 255)
+    private String nvedunom;
 }
