16eddab93eaa83b3546205388253774d0882ab93
daviidap07 <daviidap07@gmail.com>
2025-08-28 13:19:53 -0500

    models_david

--- src/main/java/com/workable_sb/workable/models/Valoracion.java
+++ src/main/java/com/workable_sb/workable/models/Valoracion.java
@@ -1,5 +1,40 @@
 package com.workable_sb.workable.models;
 
-public class Valoracion {
+import java.sql.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "valoracion")
+public class Valoracion {
+  @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer valoracion_id;
+
+	@Column(nullable = false, length = 255)
+	private String descripcion;
+
+	@Column(nullable = false)
+	private float puntuacion;
+
+	@Column(nullable = false)
+	private Date fecha_valoracion;
+
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "FK_usuario_valoracion", nullable = false)
+	private Usuario usuario;
 }
