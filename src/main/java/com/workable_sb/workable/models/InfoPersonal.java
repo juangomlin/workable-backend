b92ca06e3922bf2934b947357f26fd3d4f221d1e
NicolasZeq2722 <nicomansolov89@gmail.com>
2025-09-11 15:48:03 -0500

    Finish: InfoPersonal

--- src/main/java/com/workable_sb/workable/models/InfoPersonal.java
+++ src/main/java/com/workable_sb/workable/models/InfoPersonal.java
@@ -6,6 +6,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -21,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "info_personal")
 public class InfoPersonal {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer infoPersonal_id;
 
   @Column(nullable = false, length = 100)
