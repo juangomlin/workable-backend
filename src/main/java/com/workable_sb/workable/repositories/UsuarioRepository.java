76f88bd3af5e6ebe86852dc7d8c9e4092a139409
NicolasZeq2722 <nicomansolov89@gmail.com>
2025-09-18 14:58:42 -0500

    Finish Reclutador

--- src/main/java/com/workable_sb/workable/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.workable_sb.workable.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.workable_sb.workable.models.Usuario;
-
-public interface UsuarioRepository extends JpaRepository <Usuario, Integer> { 
-
-}
