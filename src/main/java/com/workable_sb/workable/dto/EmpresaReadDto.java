0939248024fd245dbccdc854adc83bf7b7df23dc
david <daviidap07@gmail.com>
2025-09-21 20:07:37 -0500

    rest de empresa

--- src/main/java/com/workable_sb/workable/dto/EmpresaReadDto.java
+++ src/main/java/com/workable_sb/workable/dto/EmpresaReadDto.java
@@ -1,7 +1,5 @@
 package com.workable_sb.workable.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,22 +7,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmpresaDto {
-  @NotBlank
+public class EmpresaReadDto {
+  private Integer empr_id;
+
   private String nom;
 
-  @NotNull
   private String ubi;
 
-  @NotBlank
   private String desc;
 
-  @NotNull(message = "la categoria es obligatoria")
   private Integer cate_id;
   private String nom_cat;
 
-  @NotNull(message = "el municipio es obligatorio")
   private Integer munici_id;
-
   private String nom_munici;
 }
